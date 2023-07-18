40983fe305bf5e69e0f41bddd3e51f01f94718c7
Rushmitkarki <220046@softwarica.edu.np>
2023-07-18 20:56:30 +0545

    first commit

---
+++ src/main/java/com/example/rshoesa/RShoesAApplication.java
@@ -0,0 +1,13 @@
+package com.example.rshoesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RShoesAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RShoesAApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rshoesa/RShoesAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rshoesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RShoesAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
